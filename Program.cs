08f5193fe4db4faed72d162241e170761710cbe4
AntonSol22 <soldatovanpro@gmail.com>
2023-10-23 04:54:08 +0300

    calc

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaclGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b; 
+            Console.WriteLine("Введите первое число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Выберите действие из предложенных: +, -, *, /");
+            string action = Convert.ToString(Console.ReadLine());   
+
+            switch (action)
+            {
+                case "+": 
+                    Console.WriteLine("Ответ: " + (a + b));
+                    break;
+                case "-":
+                    Console.WriteLine("Ответ: " + (a - b));
+                    break;
+                case "*":
+                    Console.WriteLine("Ответ: " + (a * b));
+                    break;
+                case "/":
+                    Console.WriteLine("Ответ: " + (a / b));
+                    break;
+            }
+        }
+    }
+}
